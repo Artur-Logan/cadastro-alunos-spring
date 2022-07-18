af4ff37e3b253ba05002677e989d0828b38e1306
Artur Logan <arturlogan1ferreira@gmail.com>
2022-07-18 20:31:15 -0300

    Commit Inicial

---
+++ src/main/java/com/artur/cadastroalunosspring/CadastroAlunosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.artur.cadastroalunosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAlunosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAlunosSpringApplication.class, args);
+	}
+
+}
